aa62cc705249f4e8be618a885b03035a924f3618
Mansi Hiremath <hiremathmansi1548@gmail.com>
2019-10-12 09:41:19 -0700

    Added project

---
+++ LoginWebApp/src/main/java/com/zensar/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.zensar.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zensar.entities.User;
+import com.zensar.services.UserService;
+
+@Controller       //request processor component
+public class UserController {
+@Autowired
+private UserService userService;
+@GetMapping("login")
+public String checkLogin(User user,ModelMap map) {
+	if(userService.validateUser(user))
+	{
+		map.addAttribute("usernm",user.getUsername());
+		return "welcome";					//logical view name
+	}
+	else {
+		map.addAttribute("logonError", "Invalid username or password");
+		return "login";
+	}
+}
+}
